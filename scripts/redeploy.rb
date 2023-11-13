e1fe72dffbf69c20d75aa73a36e8f13e8cc8506f
James Thompson <closetsolipsist@gmail.com>
2023-11-13 15:56:54 -0500

    Made deploy uses latest task definition revision * Added task-definition flag to update-service in redeploy.rb

--- scripts/redeploy.rb
+++ scripts/redeploy.rb
@@ -42,7 +42,7 @@ docker tag #{RESOURCES["IMAGE_NAME"]}:${GITHUB_SHA} #{RESOURCES["APP_IMAGE_URL"]
 docker push #{RESOURCES["APP_IMAGE_URL"]}:${GITHUB_SHA}
 docker push #{RESOURCES["APP_IMAGE_URL"]}:latest
 echo "Updating service..."
-aws ecs update-service --cluster #{RESOURCES["APP_CLUSTER"]} --service #{RESOURCES["APP_SERVICE"]} --force-new-deployment
+aws ecs update-service --cluster #{RESOURCES["APP_CLUSTER"]} --service #{RESOURCES["APP_SERVICE"]} --task-definition #{RESOURCES["APP_TASK_DEFINITION_FAMILY"]} --force-new-deployment
 echo "Waiting for service to redeploy..."
 aws ecs wait services-stable --cluster #{RESOURCES["APP_CLUSTER"]} --services #{RESOURCES["APP_SERVICE"]}
 echo "Deployment complete!"
