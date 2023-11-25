6f5ade7fc372a96538f4449b1cb158ad555d0d92
James Thompson <closetsolipsist@gmail.com>
2023-11-24 20:41:18 -0500

    Added region flag to AWS CLI commands

--- scripts/redeploy.rb
+++ scripts/redeploy.rb
@@ -42,9 +42,9 @@ docker tag #{RESOURCES["IMAGE_NAME"]}:${GITHUB_SHA} #{RESOURCES["APP_IMAGE_URL"]
 docker push #{RESOURCES["APP_IMAGE_URL"]}:${GITHUB_SHA}
 docker push #{RESOURCES["APP_IMAGE_URL"]}:latest
 echo "Updating service..."
-aws ecs update-service --cluster #{RESOURCES["APP_CLUSTER"]} --service #{RESOURCES["APP_SERVICE"]} --task-definition #{RESOURCES["APP_TASK_DEFINITION_FAMILY"]} --force-new-deployment
+aws ecs update-service --region #{RESOURCES["REGION"]} --cluster #{RESOURCES["APP_CLUSTER"]} --service #{RESOURCES["APP_SERVICE"]} --task-definition #{RESOURCES["APP_TASK_DEFINITION_FAMILY"]} --force-new-deployment
 echo "Waiting for service to redeploy..."
-aws ecs wait services-stable --cluster #{RESOURCES["APP_CLUSTER"]} --services #{RESOURCES["APP_SERVICE"]}
+aws ecs wait services-stable --region #{RESOURCES["REGION"]} --cluster #{RESOURCES["APP_CLUSTER"]} --services #{RESOURCES["APP_SERVICE"]}
 echo "Deployment complete!"
 HEREDOC
 `#{COMMAND}`
