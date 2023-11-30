5e7c72d971caac31c36aba39a1f674a537bb3ffb
James Thompson <closetsolipsist@gmail.com>
2023-11-30 15:34:02 -0500

    Creating .aws directory in authenticate.rb and fixing typo

--- scripts/authenticate.rb
+++ scripts/authenticate.rb
@@ -1,3 +1,4 @@
 require_relative './resource_names.rb'
+`mkdir ~/.aws`
 `echo "[default]\nregion = #{RESOURCES["REGION"]}\noutput = json" > ~/.aws/config`
-`echo "[default]\naws_access_key_id = #{RESOURCES["ACCESS_KEY_ID"]}\naws_secret_access_key = #{RESOURCES["SECRET_ACCESS_KEY"]}" > ~/.aws/config`
+`echo "[default]\naws_access_key_id = #{RESOURCES["ACCESS_KEY_ID"]}\naws_secret_access_key = #{RESOURCES["SECRET_ACCESS_KEY"]}" > ~/.aws/credentials`
